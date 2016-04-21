9a00ab660239947e5cb62201147152661c5d5d24
Adam Sakhr <adam.sakhr@gmail.com>
2016-04-21 15:10:05 +0200

    First Check

--- bob.js
+++ bob.js
@@ -6,9 +6,33 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+  var isSilentTreatment = function(input) {
+      return /^\s*$/.test(input);
+    };
+
+    var isShouting = function(input) {
+      return input.toUpperCase() === input && input.toLowerCase() !== input;
+    };
+
+    var isQuestion = function(input) {
+      return input.charAt(input.length - 1) === '?';
+    };
+
+    this.hey = function(input) {
+      if (isSilentTreatment(input)) {
+        return 'Fine. Be that way!';
+      }
+
+      if (isShouting(input)) {
+        return 'Whoa, chill out!';
+      }
+
+      if (isQuestion(input)) {
+        return 'Sure.';
+      }
+
+      return 'Whatever.';
+    };
 };
 
 module.exports = Bob;
